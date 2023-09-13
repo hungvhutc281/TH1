08b5375e9be17efc11e2b64bd242aae87dd62151
HÙNG ERM <114762186+hungvhutc281@users.noreply.github.com>
2023-09-13 17:24:11 +0700

    Add files via upload

---
+++ Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STUDENT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Controllers/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace STUDENT.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }//Mã sinh viên
+        public string? Name { get; set; } //Họ tên
+        public string? Email { get; set; } //Email
+        public string? Password { get; set; }//Mật khẩu
+        public Branch? Branch { get; set; }//Ngành học
+        public Gender? Gender { get; set; }//Giới tính
+        public bool IsRegular { get; set; }//Hệ: true-chính qui, false-phi cq
+        public string? Address { get; set; }//Địa chỉ
+        public DateTime DateOfBorth { get; set; }//Ngày sinh
+
+        public string AvatarPath { get; set; } // Thêm thuộc tính AvatarPath để lưu đường dẫn đến ảnh đại diệ
+    }
+}
